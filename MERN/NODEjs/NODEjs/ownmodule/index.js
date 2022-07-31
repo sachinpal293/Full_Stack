6354289ba67db7ac458761085e3fa031b57ee592
Sachin Kumar Pal <56400187+sachinpal293@users.noreply.github.com>
2022-07-31 22:16:39 +0530

    Add files via upload

---
+++ MERN/NODEjs/NODEjs/ownmodule/index.js
@@ -0,0 +1,12 @@
+const oper = require('./opera')
+// const {add,sub} = require('./opera') => object destructuring
+
+
+// console.log(add(5,5))
+// console.log(add)
+
+
+// console.log(oper)
+console.log(oper.add(5,4))
+console.log(oper.sub(10,5))
+console.log(oper.name)
---
+++ MERN/NODEjs/NODEjs/ownmodule/opera.js
@@ -0,0 +1,15 @@
+const add = (a,b) => 
+{
+  return a+b;
+};
+
+const sub = (a,b) => 
+{
+  return a-b;
+};
+const name ="Sachin kumar pal";
+
+// module.exports.add = add;
+// module.exports.sub = sub;
+
+module.exports = {add,sub,name}
