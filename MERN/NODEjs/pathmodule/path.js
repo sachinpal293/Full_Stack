4f24a62847063ed3d2fba43452d4d3abb34c078e
Sachin Kumar Pal <56400187+sachinpal293@users.noreply.github.com>
2022-08-07 23:50:36 +0530

    Delete MERN/NODEjs/pathmodule directory

--- MERN/NODEjs/pathmodule/path.js
+++
@@ -1,7 +0,0 @@
-const path = require('path')
-
-console.log(path.dirname('D:/MERN/Pathmodule/path.js'))
-const data = path.parse('D:/MERN/Pathmodule/path.js')
-
-// console.log(data['root'])
-// console.log(data.name)
